8b0d1f7a4f89e37ec9f7e2b38efc631be9248c7c
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-08 23:19:22 +0300

    The file evaluating the registration form.

---
+++ app/request/register.php
@@ -0,0 +1,33 @@
+<?php
+
+if(!empty($this->post)){
+
+    // Rule
+    $rule = array(
+        'username'          =>  'required|unique:users',
+        'password'          =>  'required|min-char:6|max-char:10'
+    );
+
+    // Message
+    $message = array(
+        'username'=>  array(
+            'required'=>'Boş bırakılmamalıdır.',
+            'unique'=>'Bu kullanıcı adı zaten var.'
+        ),        
+        'password'=>  array(
+            'required'=>'Parola belirtmeniz gerekmektedir.',
+            'min-char'=>'Minumum 6 karakter uzunluğunda bir parola belirtilmelidir.',
+            'max-char'=>'Maksimum 10 karakter uzunluğunda bir parola belirtilmelidir.'
+        )
+    );
+
+    if($this->validate($rule, $this->post, $message)){
+        if($this->insert('users', $this->post)){
+            echo 'Kayıt olduğunuz için teşekkür ederiz.';
+        } else {
+            echo 'Kayıt işleminiz başarısızlıkla sonuçlandı.';
+        }
+    } 
+}
+
+?>
