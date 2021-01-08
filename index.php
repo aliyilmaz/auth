7d5db48b4c41d4d2850d919395396a027766286f
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-09 00:08:56 +0300

    Entry route has been created.

--- index.php
+++ index.php
@@ -14,3 +14,4 @@ $Mind = new Mind($conf);
 $Mind->route('/', 'app/views/welcome');
 $Mind->route('install', 'app/migration/install');
 $Mind->route('register', 'app/views/register');
+$Mind->route('login', 'app/views/login');
