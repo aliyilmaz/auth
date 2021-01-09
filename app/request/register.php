30c046a0da330491a6bea2fe36376710820f894b
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-10 02:11:59 +0300

    The registration form has been updated with jquery.

--- app/request/register.php
+++ app/request/register.php
@@ -1,38 +1,50 @@
 <?php
 
-if(!empty($this->post)){
+$response = [];
 
-    // Rule
-    $rule = array(
-        'username'          =>  'required|unique:users',
-        'password'          =>  'required|min-char:6|max-char:10'
+// Rule
+$rule = array(
+    'username'          =>  'required|unique:users',
+    'password'          =>  'required|min-char:6|max-char:10'
+);
+
+// Message
+$message = array(
+    'username'=>  array(
+        'required'=>'Kullanıcı adı belirtmeniz gerekmektedir.',
+        'unique'=>'Başka bir kullanıcı adı belirtmeniz gerekmektedir.'
+    ),        
+    'password'=>  array(
+        'required'=>'Parola belirtmeniz gerekmektedir.',
+        'min-char'=>'Minumum 6 karakter uzunluğunda bir parola belirtilmelidir.',
+        'max-char'=>'Maksimum 10 karakter uzunluğunda bir parola belirtilmelidir.'
+    )
+);
+
+if($this->validate($rule, $this->post, $message)){
+    
+    $this->post['password'] = md5($this->post['password']);
+    $this->post['status'] = 1;
+    $this->post['created_at'] = $this->timestamp;
+
+    if($this->insert('users', $this->post)){
+        $response = array(
+            'status' => 'success',
+            'messages' => 'Kayıt olduğunuz için teşekkür ederiz.'
+        );
+    } else {
+        $response = array(
+            'status' => 'error',
+            'messages' => 'Kayıt işleminiz başarısızlıkla sonuçlandı.'
+        );
+    }
+} else {
+    $response = array(
+        'status' => 'error',
+        'messages' => $this->errors
     );
+} 
 
-    // Message
-    $message = array(
-        'username'=>  array(
-            'required'=>'Kullanıcı adı belirtmeniz gerekmektedir.',
-            'unique'=>'Başka bir kullanıcı adı belirtmeniz gerekmektedir.'
-        ),        
-        'password'=>  array(
-            'required'=>'Parola belirtmeniz gerekmektedir.',
-            'min-char'=>'Minumum 6 karakter uzunluğunda bir parola belirtilmelidir.',
-            'max-char'=>'Maksimum 10 karakter uzunluğunda bir parola belirtilmelidir.'
-        )
-    );
-
-    if($this->validate($rule, $this->post, $message)){
-        
-        $this->post['password'] = md5($this->post['password']);
-        $this->post['status'] = 1;
-        $this->post['created_at'] = $this->timestamp;
-
-        if($this->insert('users', $this->post)){
-            echo 'Kayıt olduğunuz için teşekkür ederiz.';
-        } else {
-            echo 'Kayıt işleminiz başarısızlıkla sonuçlandı.';
-        }
-    } 
-}
+echo json_encode($response);
 
 ?>
--- index.php
+++ index.php
@@ -17,3 +17,6 @@ $Mind->route('register', 'app/views/register');
 $Mind->route('login', 'app/views/login');
 $Mind->route('logout', 'app/request/logout');
 $Mind->route('dashboard', 'app/views/dashboard');
+
+$Mind->route('api/register', 'app/request/register');
+
