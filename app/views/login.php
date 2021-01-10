d59e0704d48d0159245e4203819dc9ed7e3548dc
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-10 03:00:06 +0300

    The codes have been combined.

--- app/views/login.php
+++ app/views/login.php
@@ -1,3 +1,4 @@
+<base href="<?=$this->base_url;?>">
 <h1>Giriş</h1>
 <form id="login-form">
 <input type="text" name="username" placeholder="Username">
@@ -12,44 +13,6 @@ h4.status{
     display:none;
 }
 </style>
-<script>
-window.addEventListener('load', (event) => {
 
-    $('button#login-button').on('click', function(){
-        let form = $('form#login-form');
-        form.find('.status').html('');
-        $.post('api/login', form.serializeArray(), function(response){
-
-			response 	        = JSON.parse(response);
-			requestStatus 	    = response.status;
-			requestMessages 	= response.messages;
-
-            switch (requestStatus) {
-                case 'success':
-                    form.find('.status').html(requestMessages).show();
-                    form.trigger('reset');
-                    setTimeout(() => {
-                        window.location = 'dashboard';
-                    }, 1000);
-                break;
-                case 'error':
-                    if( Object.prototype.toString.call(requestMessages) == '[object String]' ) {
-						form.find('.status').append('<strong style="color:red;">*</strong> '+requestMessages+'<br />').show();
-					} else {
-						$.each( requestMessages, function( name, values ) {
-							$.each( values, function( rule, value ) {
-								form.find('.status').append('<strong style="color:red;">*</strong> '+value+'<br />').show();
-							});
-	
-						});
-					}
-                break;
-            }
-
-        });
-    });
-
-    
-});
-</script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="public/assets/js/custom.js"></script>
--- app/views/register.php
+++ app/views/register.php
@@ -1,3 +1,4 @@
+<base href="<?=$this->base_url;?>">
 <h1>Kayıt</h1>
 <form id="register-form">
 <input type="text" name="username" placeholder="Username">
@@ -12,41 +13,6 @@ h4.status{
     display:none;
 }
 </style>
-<script>
-window.addEventListener('load', (event) => {
 
-    $('button#register-button').on('click', function(){
-        let form = $('form#register-form');
-        form.find('.status').html('');
-        $.post('api/register', form.serializeArray(), function(response){
-
-			response 	        = JSON.parse(response);
-			requestStatus 	    = response.status;
-			requestMessages 	= response.messages;
-
-            switch (requestStatus) {
-                case 'success':
-                    form.find('.status').html(requestMessages).show();
-                    form.trigger('reset');
-                break;
-                case 'error':
-                    if( Object.prototype.toString.call(requestMessages) == '[object String]' ) {
-						form.find('.status').append('<strong style="color:red;">*</strong> '+requestMessages+'<br />').show();
-					} else {
-						$.each( requestMessages, function( name, values ) {
-							$.each( values, function( rule, value ) {
-								form.find('.status').append('<strong style="color:red;">*</strong> '+value+'<br />').show();
-							});
-	
-						});
-					}
-                break;
-            }
-
-        });
-    });
-
-    
-});
-</script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="public/assets/js/custom.js"></script>
